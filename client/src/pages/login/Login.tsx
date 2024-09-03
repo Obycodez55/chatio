9e9af5dea8eec5473c4c0f50b81ecca25e84c275
Obikoya Adebayo <obikoyaadebayo55@gmail.com>
2024-09-03 19:27:53 +0100

    Created login page

--- client/src/pages/login/Login.tsx
+++ client/src/pages/login/Login.tsx
@@ -1,11 +1,67 @@
-import React from 'react'
+import React from "react";
+import { IoMdEye, IoMdEyeOff } from "react-icons/io";
 
-type Props = {}
+type Props = {};
 
 const Login = (props: Props) => {
+  const [passwordOpen, setPasswordOpen] = React.useState(false);
   return (
-    <div>Login</div>
-  )
-}
+    <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+      <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+        <h1 className="text-3xl font-semibold text-center text-gray-300">
+          Login
+          <span className="text-blue-500"> Chatio</span>
+        </h1>
+        <form>
+          <div>
+            <label htmlFor="email" className="label p-2">
+              <span className="text-base label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              id="email"
+              placeholder="Enter your email..."
+              className="input input-bordered w-full h-10"
+            />
+          </div>
 
-export default Login
+          <div>
+            <label htmlFor="password" className="label p-2">
+              <span className="text-base label-text">Password</span>
+            </label>
+            <div className="flex">
+              <input
+                type={passwordOpen ? "text" : "password"}
+                id="password"
+                placeholder="Enter your password..."
+                className="input input-bordered w-full h-10"
+              />
+              <div className="absolute right-8 h-10 flex items-center justify-center cursor-pointer"
+                onClick={() => setPasswordOpen(!passwordOpen)}
+              >
+                {passwordOpen ? (
+                  <IoMdEyeOff className="w-6 h-6" />
+                ) : (
+                  <IoMdEye className="w-6 h-6" />
+                )}
+              </div>
+            </div>
+          </div>
+
+          <a
+            href="#"
+            className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block"
+          >
+            Don&apos;t have an account? Register
+          </a>
+
+          <button type="submit" className="btn btn-block btn-sm mt-2">
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
